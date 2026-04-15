a4dc0b2bd156e3614e19eae8c7f178c36b7476f7
asiduki <147263246+asiduki@users.noreply.github.com>
2026-04-15 13:09:43 +0530

    fix room history

--- server/server.js
+++ server/server.js
@@ -13,7 +13,7 @@ import convertRoutes from  "./routes/convertRoutes.js";
 
 app.use(
     cors({
-        origin: ["http://localhost:5173","https://collaborative-code-editor-pearl.vercel.app "],
+        origin: ["http://localhost:5173","https://collaborative-code-editor-pearl.vercel.app"],
         credentials: true,
     })
 );
